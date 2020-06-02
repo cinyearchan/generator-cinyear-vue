417cd71a87d2e471cfe3bdc3a5d826d8cb7250d7
cinyearchan <cinyearchan@gmail.com>
2020-06-02 11:33:21 +0800

    feat: initial commit:

---
+++ generators/app/index.js
@@ -0,0 +1,57 @@
+const Generator = require('yeoman-generator')
+
+module.exports = class extends Generator {
+  prompting () {
+    return this.prompt([
+      {
+        type: 'input',
+        name: 'name',
+        message: 'Your project name',
+        default: this.appname
+      }
+    ])
+      .then(answers => {
+        this.answers = answers
+      })
+  }
+
+  writing () {
+    // 把每一个文件都通过模板转换到目标路径
+    const templates = [
+      '.browserslistrc',
+      '.editorconfig',
+      '.env.production',
+      '.eslintrc.js',
+      '.gitignore',
+      'babel.config.js',
+      'package.json',
+      'postcss.config.js',
+      'README.md',
+      // 'yarn.lock',
+      'public/favicon.ico',
+      'public/index.html',
+      'src/App.vue',
+      'src/main.js',
+      'src/router.js',
+      'src/assets/logo.png',
+      'src/components/HelloWorld.vue',
+      'src/store/actions.js',
+      'src/store/getters.js',
+      'src/store/index.js',
+      'src/store/mutations.js',
+      'src/store/state.js',
+      'src/utils/request.js',
+      'src/views/About.vue',
+      'src/views/Home.vue'
+    ]
+
+    // 根据模板及其对应路径，在目标项目内生成对应路径下的对应文件
+    templates.forEach(item => {
+      this.fs.copyTpl(
+        this.templatePath(item),
+        this.destinationPath(item),
+        this.answers
+      )
+    })
+  }
+}
